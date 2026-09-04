58462d119f55737922499522072062a0b5057a34
jovince2004 <c20245853.delacruz@csav.edu.ph>
2026-09-04 17:32:57 +0800

    addEventListener

--- script.js
+++ script.js
@@ -21,13 +21,34 @@ function greet(greeting, names = []) {
 console.log(greet("Mayung aga", ["Jovince", "Bai", "Soy" , "tol" , "erp"]));
 
 const heading = document.querySelector("h1 ");
-heading.textContent = "Mel 'amay fish dealer' Toyab";
+heading.textContent = "Mel 'amay fish dealer🐟' Toyab";
 heading.style.color = "gray";
 heading.style.fontSize = "50px";
 heading.style.backgroundColor = "yellow";
+console.log(heading);
+
+heading.addEventListener("click", function() {
+    heading.style.color = "red";
+    console.log("Heading clicked!");
+});
 
 const contactHeading = document.querySelector("#contact h2");
 console.log(contactHeading);
 
 const serviceHeading = document.querySelector("#services h2");
 console.log(serviceHeading);
+
+const toggleButton = document.querySelector("#switch");
+const body = document.querySelector("body");
+let isDarkMode = false;
+
+toggleButton.addEventListener("click", function() {
+    isDarkMode = !isDarkMode;
+    if (isDarkMode) {
+        body.style.backgroundColor = "black"; 
+        body.style.color = "white";  
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    }
+});
