9652e9a886e4330692a0e4c9c8a1fc2d6dbb98cd
jovince2004 <c20245853.delacruz@csav.edu.ph>
2026-09-04 14:41:04 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log("Hello world!");
 console.log("From script.js File");
+
+const myName = "Jovince";
+let age = 20;
+let number = 9068687904;
+let address = "Hda. LA Milagrosa";
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Contact number: ${number}`);
+console.log(`Address: ${address}`);
+console.log(67);
